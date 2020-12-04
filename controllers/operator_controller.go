0fc5a3d27dfa39a4a11e7b5cd27d1d1c40893776
raffaele <raffyt@hotmail.it>
2020-12-04 14:48:53 +0100

    initial implementation

--- controllers/operator_controller.go
+++ controllers/operator_controller.go
@@ -18,13 +18,20 @@ package controllers
 
 import (
 	"context"
-
+	"fmt"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+	"github.com/vishvananda/netlink"
 	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	batchv1 "operator-example/api/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-
-	webappv1 "operator-example/api/v1"
 )
 
 // OperatorReconciler reconciles a Operator object
@@ -38,34 +45,80 @@ type OperatorReconciler struct {
 // +kubebuilder:rbac:groups=webapp.my.domain,resources=operators/status,verbs=get;update;patch
 
 func (r *OperatorReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("operator", req.NamespacedName)
+	ctx := context.Background()
+	log := r.Log.WithValues("operator", req.NamespacedName)
 
-	// your logic here
+	p := &corev1.Pod{}
+	if err := r.Get(ctx, req.NamespacedName, p); err != nil {
+		log.Error(err, "unable to fetch pod")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	//ottengo la lista di link (in quale namespace???)
+	linkList, _ := netlink.LinkList()
+	vethLink := linkList[len(linkList)]
+	brExists := false
+	var brLink netlink.Link
+	//cerco se esiste già il bridge
+	for _, link := range linkList {
+		if link.Attrs().Name == "br1" {
+			brExists = true
+			brLink = link
+			break
+		}
+	}
+	//il bridge non esiste --> lo creo e gli attacco la veth --> NOTA: per il momento gli attacco l'ultima veth creata
+	if brExists != true {
+		la := netlink.NewLinkAttrs()
+		la.Name = "foo"
+		mybridge := &netlink.Bridge{LinkAttrs: la}
+		err := netlink.LinkAdd(mybridge)
+		if err != nil  {
+			fmt.Printf("could not add %s: %v\n", la.Name, err)
+		}
+		netlink.LinkSetMaster(vethLink, mybridge)
+	} else {
+		//il bridge esiste, attacco semplicemente la veth pair al bridge
+		netlink.LinkSetMaster(vethLink, brLink)
+	}
 
 	return ctrl.Result{}, nil
 }
 
 func (r *OperatorReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	c, err := ctrl.NewControllerManagedBy(mgr).For(&webappv1.Operator{}).Build(r)
+	c, err := ctrl.NewControllerManagedBy(mgr).For(&batchv1.Operator{}).Build(r)
 
-	if err == nill && c != nil {
+	if err == nil && c != nil {
 		errWatch := c.Watch(
-			&source.Kind{Type: &appsv1.Pod{}),
-			handler.EnqueueRequestsFromMapFunc(func(a client.Object) []reconcile.Request {
-        			return []reconcile.Request{
-            				{NamespacedName: types.NamespacedName{
-                			Name: a.GetName(),
-                			Namespace: "default",
-            				}},
-			}),
+			&source.Kind{Type: &corev1.Pod{}},
+			&handler.EnqueueRequestsFromMapFunc{
+				ToRequests: handler.ToRequestsFunc(func(a handler.MapObject) []reconcile.Request {
+
+					return []reconcile.Request{
+						{NamespacedName: types.NamespacedName{
+							Name:      a.Meta.GetName(),
+							Namespace: "default",
+						}},
+					}
+				}),
+			},
 			predicate.Funcs{
-				CreateFunc: func(e event.CreateEvent) {
-					
+				CreateFunc: func(e event.CreateEvent) bool {
+					annotations := e.Meta.GetAnnotations()
+					for key, element := range annotations {
+						if key == "k8s.v1.cni.cncf.io/network" {
+							fmt.Println("Multus annotation: ", element)
+							return true
+						}
+					}
+					return false
+				},
+			},
+		)
 		if errWatch != nil {
-			fmt.Println("watch non settato")
+			fmt.Println("setted watch")
 		} else {
-			fmt.Println("watch settato")
+			fmt.Println("watch not setted")
 		}
 	}
 	return err
